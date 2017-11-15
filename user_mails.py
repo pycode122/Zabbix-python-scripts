1f28ef99a800530375e1fb114df63195858f519c
Abhiram <t.abhiram7@gmail.com>
2017-11-15 18:20:32 +0530

    Add files via upload

---
+++ user_mails.py
@@ -0,0 +1,34 @@
+
+############## Users added in zabbix with the required mail(users media) extension###########
+
+###in this example I am retriving for @sap.com############
+
+
+f0rom pyzabbix import ZabbixAPI
+import re
+import sys,logging
+import json
+import  csv
+
+zapi = ZabbixAPI("http://ip/zabbix")
+zapi.login("user", "password")
+zapi.timeout = 300
+'''
+stream = logging.StreamHandler(sys.stdout)
+stream.setLevel(logging.DEBUG)
+
+log = logging.getLogger('pyzabbix')
+log.addHandler(stream)
+log.setLevel(logging.DEBUG)
+'''
+print("Connected to Zabbix API Version %s" % zapi.api_version())
+
+users = zapi.user.get(output=['userid','alias'])
+
+for u in users: 
+	usermedia = zapi.usermedia.get(userids=u['userid'],output=['sendto','active'])
+	for i in usermedia:
+		if re.search ('.*\@sap.com',i['sendto']):
+			print ()
+			print ("Email :",i['sendto']," ---- ZabbixUser:",u['alias'])
+			
